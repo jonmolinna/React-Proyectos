952b18829624372ac90d9d888ad549c6b3d643d5
jonmolinna <jmolina2624@gmail.com>
2021-08-20 16:52:05 -0500

    Messenger Clone

--- messenger/web/src/App.js
+++ messenger/web/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './componentes/Login';
+import Message from './componentes/Message';
 
 function App() {
+  const [name, setName] = useState('Jon Dalla');
+
   return (
     <div className="app">
+      {
+        !name? (
+          <Login />
+        ) : (
+          <Message />
+        )
+      }
     </div>
   );
 }
