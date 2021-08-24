500b112b0e06c86e6b215a37a4232657d91ee20b
jonmolinna <jmolina2624@gmail.com>
2021-08-24 09:09:51 -0500

    Messenger Clone

--- messenger/backend/src/index.js
+++ messenger/backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import Pusher from 'pusher';
 
 import chatRutas from './chat.router.js';
 
@@ -8,21 +9,36 @@ import chatRutas from './chat.router.js';
 const app = express();
 const port = process.env.PORT || 9000;
 
+const pusher = new Pusher({
+    appId: "1254899",
+    key: "67142f14d3c0d4f4ff22",
+    secret: "030af22c706e578eadb4",
+    cluster: "us2",
+    useTLS: true
+});
+
 // middlewares
 app.use(express.json());
 app.use(cors());
 
-const URI = 'mongodb://localhost/messenger_project';
+//const URI = 'mongodb://localhost/messenger_project';
+const URI = 'mongodb+srv://admin:5sBtzVQVEajo92re@cluster0.qsb0w.mongodb.net/messengerDB?retryWrites=true&w=majority';
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
-const db = mongoose.connection;
-
-db.once('open', () => {
+mongoose.connection.once('open', () => {
     console.log('DB Connected');
+
+    const changeStream = mongoose.connection.collection('messages').watch()
+    changeStream.on('change', (change) => {
+        pusher.trigger('messages', 'newMessage', {
+            'change': change
+        });
+    });
 });
 
 // Rutas
