22605ad5c2ab167379dfb9d5895db5a3f02c3b69
jonmolinna <jmolina2624@gmail.com>
2021-09-10 15:29:54 -0500

    Whatsapp Clone

--- whatsapp/web/src/helpers/axios.js
+++ whatsapp/web/src/helpers/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000/api/message'
+    baseURL: 'https://whatsapp-proyecto-clone.herokuapp.com/api/message'
+
 });
 
 export default instance;
+
+//'http://localhost:9000/api/message'
