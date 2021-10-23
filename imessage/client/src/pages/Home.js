92271c927c4b1707cb3e4fd4fe5bf764c2a9f4a9
jonmolinna <jmolina2624@gmail.com>
2021-10-23 14:10:20 -0500

    Pages

---
+++ imessage/client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+// import { useAuthState } from '../context/auth';
+
+const Home = () => {
+
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    )
+}
+
+export default Home
