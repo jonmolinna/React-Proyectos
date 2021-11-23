09fb613f759728430604e171222eea5089a7aeb7
jonmolinna <jmolina2624@gmail.com>
2021-11-23 15:03:47 -0500

    Elimino la carpeta client de Imessage

--- imessage/client/src/util/capitalize.js
+++
@@ -1,8 +0,0 @@
-function Capitalize(words) {
-    return words.toLowerCase()
-        .split(' ')
-        .map((word) => word[0].toUpperCase() + word.substr(1))
-        .join(' ');
-};
-
-export default Capitalize;
